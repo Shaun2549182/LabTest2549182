6d668b31804899f2fb4634e24d7188cd9606e812
Shaun2549182 <2549182@students.wits.ac.za>
2026-04-17 16:39:49 +0200

    Added testscript.py for testing. Added test_NoMarkColumn.csv. Implemented fix for lack of mark column in .csv file.

---
+++ tests/testscript.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+
+def getCols(f):
+    ''' Identify the columns that contain the marks and student numbers '''
+    headings = f.readline().strip().split(",") # Make a list headings, reading the first line of the csv, seperating by commas. 
+    mark_col = None
+    i = 0 # The starting value for the for loop to iterate through
+    for head in headings: #Iterate through the size of the list headings
+        if head == "Student Number": 
+            num_col = i # When the string Student Number is found, set the value of num_col to i
+        elif head == "Mark" :
+            mark_col = i # When the string Mark is found, set the value of mark_col to i
+        i += 1 # Increment i each time the for loop repeats
+
+        if mark_col is None:
+            raise ValueError("Could not find Mark column in file")
+    return (num_col, mark_col)
+
+def findTop(f,num_col, mark_col):
+    ''' finds the top student in the class '''
+    best = best_idx = -1 # Initialize best and best_idx to -1 as 0 is a possible mark
+    for line in f:
+        data = line.strip().split(",")
+        mark = int(data[mark_col]) # Set integer value Mark to value of mark_col in data list
+        if mark > best: # If current mark is better than previous best mark, update best mark to current mark, and student number to current position in number column
+            best=mark
+            best_idx = data[num_col]
+    return best_idx, best
+
+f = open(sys.argv[1]) # Prompt user for input file name
+num_col, mark_col = getCols(f) # Set values of num_col and mark_col using getCols() function
+best_idx, best = findTop(f,num_col,mark_col) # Set values of best_idx and best using findTop() function
+print("The top student was student number %s with mark %d"%(best_idx,best))
