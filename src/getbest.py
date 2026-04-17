57726cf995accd16f74829ddb08a252f7e0c7690
Shaun2549182 <2549182@students.wits.ac.za>
2026-04-17 16:56:37 +0200

    Added error handling to source file

--- src/getbest.py
+++ src/getbest.py
@@ -6,12 +6,18 @@ def getCols(f):
     ''' Identify the columns that contain the marks and student numbers '''
     headings = f.readline().strip().split(",") # Make a list headings, reading the first line of the csv, seperating by commas. 
     i = 0 # The starting value for the for loop to iterate through
+    mark_col = None
+    num_col = None
     for head in headings: #Iterate through the size of the list headings
         if head == "Student Number": 
             num_col = i # When the string Student Number is found, set the value of num_col to i
         elif head == "Mark" :
             mark_col = i # When the string Mark is found, set the value of mark_col to i
         i += 1 # Increment i each time the for loop repeats
+    if mark_col is None:
+        raise ValueError("Could not find Mark column in file")
+    elif num_col is None:
+       raise ValueError("Could not find Student Number column in file")
     return (num_col, mark_col)
 
 def findTop(f,num_col, mark_col):
@@ -23,6 +29,8 @@ def findTop(f,num_col, mark_col):
         if mark > best: # If current mark is better than previous best mark, update best mark to current mark, and student number to current position in number column
             best=mark
             best_idx = data[num_col]
+    if best > 100:
+        raise ValueError("Highest mark above 100")
     return best_idx, best
 
 f = open(sys.argv[1]) # Prompt user for input file name
